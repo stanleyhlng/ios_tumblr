25265ca0aeab138fff494eee59ba188f94fc5d9d
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-24 22:12:53 -0700

    Initial Commit

---
+++ ios_tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_tumblr
+//
+//  Created by Stanley Ng on 6/24/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_tumblr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios_tumblr
+//
+//  Created by Stanley Ng on 6/24/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
