ff6debd46e49a5273896de8a085caa3dc51edfa8
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-29 01:02:29 -0700

    updated

--- ios_tumblr/DashboardViewController.m
+++ ios_tumblr/DashboardViewController.m
@@ -12,13 +12,18 @@
 @interface DashboardViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *loadingImageView;
-@property (weak, nonatomic) IBOutlet UIImageView *textImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *homeImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *homeSpinnerImageView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIView *homeProxyView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 - (void)customizeTitleView;
 - (void)customizeRightBarButton;
 - (void)handleLoginButtonTap;
 - (void)handleLoading;
-- (void)showText;
+- (void)showContent;
+- (void)updateContent;
 
 @end
 
@@ -49,9 +54,12 @@
     [self.view setBackgroundColor:[AVHexColor colorWithHexString:@"#33465d"]];
 
     [self.navigationController.navigationBar setTranslucent:NO];
-    
+
+    self.homeImageView.image = [self.homeImageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    self.homeImageView.tintColor = [AVHexColor colorWithHexString:@"#ffffff"];
+
     [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(handleLoading) userInfo:nil repeats:NO];
-    [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(showText) userInfo:nil repeats:NO];
+    [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(showContent) userInfo:nil repeats:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -102,16 +110,49 @@
     [self.loadingImageView startAnimating];
 }
 
-- (void)showText
+- (void)showContent
 {
-    self.textImageView.hidden = NO;
-    self.textImageView.alpha = 0;
+    self.loadingImageView.hidden = YES;
+
+    // text
+    self.containerView.hidden = NO;
+    self.containerView.alpha = 0;
     [UIView animateWithDuration:0.2f
                           delay:0.0f
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
-                         self.textImageView.alpha = 1.0f;
+                         self.containerView.alpha = 1.0f;
                      }
                      completion:nil];
+
+    // home
+    self.homeImageView.hidden = NO;
+    self.homeImageView.transform = CGAffineTransformMakeScale(0.5f, 0.5f);
+    [UIView animateWithDuration:1.0f
+                          delay:0.0f
+         usingSpringWithDamping:0.6f
+          initialSpringVelocity:0.8f
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         self.homeImageView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+                     }
+                     completion:^(BOOL finished) {
+                         
+                         self.homeSpinnerImageView.hidden = NO;
+                         self.homeSpinnerImageView.transform = CGAffineTransformMakeScale(0.0f, 0.0f);
+                         [UIView animateWithDuration:0.2f
+                                          animations:^{
+                                              self.homeSpinnerImageView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+                                          }];
+                         
+                     }];
+    
+    [NSTimer scheduledTimerWithTimeInterval:3 target:self selector:@selector(updateContent) userInfo:nil repeats:NO];
+}
+
+- (void)updateContent
+{
+    self.homeImageView.tintColor = [AVHexColor colorWithHexString:@"#aeb5be"];
+    self.titleLabel.textColor = [AVHexColor colorWithHexString:@"#aeb5be"];
 }
 @end
